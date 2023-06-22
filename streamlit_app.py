e8caf93db5c778bd90029a0efe71d1deaff09e50
sony0700 <137334604+sony0700@users.noreply.github.com>
2023-06-22 10:34:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('hello welcome to the page')
 streamlit.header('Breakfast Menu')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🥣 🥗 🍞Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥣  🥑🍞Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥣 🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
