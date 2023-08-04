119248df17f638ea1eeee558a87622bb93875b5a
sony0700 <137334604+sony0700@users.noreply.github.com>
2023-08-04 14:36:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,10 +21,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 #create a repeatable code called function
+
  def get_fruityvice_data(this_fruit_choice)
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +this_fruit_choice)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      return fruityvice_normalized)
+      return fruityvice_normalized
      
 streamlit.header('FruityVice fruit Advice')
 try:
